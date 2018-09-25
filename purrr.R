556028ec0ba1075c07f60dc9ea513b93934bd72c
Tom Curnock <tom@greenlightre.ky>
2018-09-25 17:26:07 -0500

    pluck me

--- purrr.R
+++ purrr.R
@@ -1,5 +1,6 @@
 library(purrr)
 library(magrittr)
+library(rlang)
 
 # reduce
 # reduce() combines from the left, reduce_right() combines from the right. reduce(list(x1, x2, x3), f)
@@ -58,40 +59,81 @@ reduce(samples, intersect)
 
 
 
-# functional programning
-Funcall <- function(f, ...) f(...)
-## Compute log(exp(acos(cos(0))))
-Reduce(Funcall, list(log, exp, acos, cos), 5, right = TRUE)
-## n-fold iterate of a function, functional style:
-Iterate <- function(f, n = 1) function(x) Reduce(Funcall, rep.int(list(f), n), x, right = TRUE)
-## Continued fraction approximation to the golden ratio:
-Iterate(function(x) 1 + 1 / x, 30)(1)
+# as_mapper
+# FORMULA
+f <- as_mapper(~print(.y))
+f # gives function (..., .x = ..1, .y = ..2, . = ..1) print(.y)
+f(3, 4, 5)
 
-reduce_right(list(log, exp, acos, cos), Funcall, .init = 5)
+f <- as_mapper(~print(..3))
+f(3, 4, 5)
 
-
-
-reduce_right(list(cos), Funcall, .init = 0)
-
+f <- as_mapper(~print(list2(...)))
+vars <- list(x = 1, y = 2)
+name <- "z"
+f(w = 0, !!!vars, !!name := 3)
 
 
+# FUNCTION
+# functions used as is
+f <- as_mapper(mean)
+f
+f(3:5)
+
+# CHARACTER VECTOR, NUMERIC VECTOR, LIST
+# conveted to extractor function
+f <- as_mapper(list(1, 2))
+f # give pluck(x, list(1, 2), .default = NULL), which is same as x[[1]][[2]]
+l <- list(a = list(u = 1, v = 2), b = list(x = 3, y = 4))
+f(l) # gives 2
+
+f <- as_mapper(1:2)
+f
+f(l) # gives 2
+
+f <- as_mapper(c("a", "v"))
+f(l) # gives 2
 
 
 
 
 
+# pluck
+obj1 <- list("a", list(1, elt = "foobar"))
+obj2 <- list("b", list(2, elt = "foobaz"))
+x <- list(obj1, obj2)
+x
+
+# And now an accessor for these complex data structures:
+my_element <- function(x) x[[2]]$elt
+
+# The accessor can then be passed to pluck:
+pluck(x, 1, my_element)
+pluck(x, 2, my_element)
 
 
 
+# This technique is used for plucking into attributes with
+# attr_getter(). It takes an attribute name and returns a function
+# to access the attribute:
+obj1 <- structure("obj", obj_attr = "foo")
+obj2 <- structure("obj", obj_attr = "bar")
+x <- list(obj1, obj2)
+
+# pluck() is handy for extracting deeply into a data structure.
+# Here we'll first extract by position, then by attribute:
+pluck(x, 1, attr_getter("obj_attr"))  # From first object
+pluck(x, 2, attr_getter("obj_attr"))  # From second object
+
+
+# pluck() splices lists of arguments automatically. The following
+# pluck is equivalent to the one above:
+idx <- list(1, attr_getter("obj_attr"))
+pluck(x, idx)
 
 
 
-
-
-
-
-
-
+v <- list(1:2, 3:4, 5:6) %>% as_vector(integer(2))
 
 
 
@@ -105,6 +147,36 @@ reduce_right(list(cos), Funcall, .init = 0)
 # accumulate(.x, .f, ..., .init)
 # accumulate_right(.x, .f, ..., .init)
 
+1:3 %>% accumulate(`+`)
+1:10 %>% accumulate(max, .init = 5)
+c(5,1:10) %>% accumulate(max)
+
+
+1:10 %>% accumulate(~ .x)
+1:10 %>% accumulate(~ .y)
+
+
+
+1:10 %>% reduce(~ .x)
+1:10 %>% reduce(~ .y)
+
+
+ff <- as_mapper(~ .x)
+ff
+
+
+gg <- as_mapper(~ .y)
+gg
+gg(1, 2)
+gg(2, 3)
+
+hh <- as_mapper(~ x ^2)
+hh
+
+tt <- function(...) {
+  print(..1)
+}
+tt(3,4,5)
 
 
 
@@ -114,19 +186,4 @@ tt
 
 
 as_mapper(list(1, attr_getter("a")))
-?pluck
 
-
-
-
-obj1 <- list("a", list(1, elt = "foobar"))
-obj2 <- list("b", list(2, elt = "foobaz"))
-x <- list(obj1, obj2)
-x
-
-# And now an accessor for these complex data structures:
-my_element <- function(x) x[[2]]$elt
-
-# The accessor can then be passed to pluck:
-pluck(x, 1, my_element)
-pluck(x, 2, my_element)
